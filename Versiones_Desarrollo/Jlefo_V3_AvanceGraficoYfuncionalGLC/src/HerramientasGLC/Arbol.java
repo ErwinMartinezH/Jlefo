ebcae998fc3ed6d622bec10892a04f40e62b8114
EBMH <brownherrera11@gmail.com>
2023-06-17 13:56:04 -0600

    Uptdate Arboles_Derivacion

---
+++ Versiones_Desarrollo/Jlefo_V3_AvanceGraficoYfuncionalGLC/src/HerramientasGLC/Arbol.java
@@ -0,0 +1,16 @@
+package HerramientasGLC;
+
+import javax.swing.*;
+
+public class Arbol {
+    public Arbol(String[][] datos) {//recibe los datos de la gramatica
+        //Toma los valores de la tabla y los guarda en un arbol de derivacion para dibujarlo
+        
+    }
+
+    public void crearArbol(JPanel panel3) {//crea el arbol de derivacion
+    //dibuja el arbol de derivacion en el panel3 del frame de la ventana, toma los valores
+
+
+    }
+}
